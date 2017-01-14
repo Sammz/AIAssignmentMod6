80d3803a9fb20ae9ff8a48542c240b4358a9539f
Sam <sam.witt25@gmail.com>
2017-01-14 14:51:03 +0100

    die vage errors opgelost, veranderd in weakwarnings

--- src/main/java/nl/epicspray/AI/Main.java
+++ src/main/java/nl/epicspray/AI/Main.java
@@ -1,10 +1,12 @@
 package nl.epicspray.AI;
 
+import nl.epicspray.AI.util.SystemController;
+
 /**
  * Created by Gebruiker on 14-1-2017.
  */
 public class Main {
     public static void main(String[] args) {
-
+        SystemController.getLogger().debug("Test");
     }
 }
