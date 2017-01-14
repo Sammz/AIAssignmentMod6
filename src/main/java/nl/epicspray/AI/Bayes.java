931636cc5c27a928134121eccc1ca8506bf570ea
DeltaNuke <pelledegreeuw97@gmail.com>
2017-01-14 15:32:48 +0100

    Added stub bayes

---
+++ src/main/java/nl/epicspray/AI/Bayes.java
@@ -0,0 +1,14 @@
+package nl.epicspray.AI;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Gebruiker on 14-1-2017.
+ */
+public class Bayes {
+
+    public void Train(List<String> classes, Map<Map<String, Integer>, String> docs){
+        
+    }
+}
