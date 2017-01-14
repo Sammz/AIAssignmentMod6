ae5c58fe0f248905d2a19248fcba9451d2378bab
DeltaNuke <pelledegreeuw97@gmail.com>
2017-01-14 17:29:21 +0100

    Commit bayes

--- src/main/java/nl/epicspray/AI/Bayes.java
+++ src/main/java/nl/epicspray/AI/Bayes.java
@@ -1,14 +1,99 @@
 package nl.epicspray.AI;
 
-import java.util.List;
-import java.util.Map;
+import nl.epicspray.AI.util.SystemController;
+import sun.plugin2.gluegen.runtime.CPU;
+
+import java.util.*;
 
 /**
  * Created by Gebruiker on 14-1-2017.
  */
 public class Bayes {
 
-    public void Train(List<String> classes, Map<Map<String, Integer>, String> docs){
-        
+    private Set<String> V = new HashSet<String>();
+    private Map<String, Double> CPrior;
+    private List<String> C;
+    private Map<Map<String, Integer>, String> D;
+    private Map<String, Map<String, Double>> chanceOnWordGivenClass;
+    private int K = 1;
+
+    public void train(List<String> classes, Map<Map<String, Integer>, String> docs){
+        C = classes;
+        D = docs;
+        for(Map<String, Integer> map : D.keySet()){
+            V.addAll(map.keySet());
+        }
+        Map<String, Integer> totalDocsPerClass = new HashMap<String, Integer>();
+        Map<String, Map<String, Integer>> countWordsPerClass = new HashMap<String, Map<String, Integer>>();
+        chanceOnWordGivenClass = new HashMap<String, Map<String, Double>>();
+        Map<String, Integer> totalWordsPerClass = new HashMap<String, Integer>();
+        //Total classes
+        for(String c : C){
+            totalDocsPerClass.put(c, 0);
+            countWordsPerClass.put(c, new HashMap<String, Integer>());
+            chanceOnWordGivenClass.put(c, new HashMap<String, Double>());
+            totalWordsPerClass.put(c, 0);
+            SystemController.getLogger().debug("Found class: " + c);
+        }
+
+        //Map every word to a class instead of only a document
+        int totalDocs = 0;
+        for(String s : C){
+            for(Map<String, Integer> map : D.keySet()){
+                SystemController.getLogger().debug("Doc found with class: " + D.get(map) + " " + s + " " + s.equals(D.get(map)));
+                if(s.equals(D.get(map))){
+                    totalDocs++;
+                    totalDocsPerClass.put(s, totalDocsPerClass.remove(s) + 1);
+                    SystemController.getLogger().debug("Found Doc with class: " + s + ", total : " + totalDocsPerClass.get(s));
+                    for(String c : map.keySet()){
+                        if(countWordsPerClass.get(s).containsKey(c)){
+                            countWordsPerClass.get(s).put(c, countWordsPerClass.get(s).remove(c) + 1);
+                        } else {
+                            countWordsPerClass.get(s).put(c, 1);
+                        }
+                    }
+                }
+            }
+        }
+        CPrior = new HashMap<String, Double>();
+        for(String c : C){
+            CPrior.put(c, ((double) totalDocsPerClass.get(c) / totalDocs));
+            SystemController.getLogger().debug("Calculated prior chance for class: " + c + " = " + CPrior.get(c));
+        }
+
+        //Calculate total amount of words
+        for(String c : C){
+            for(String s : countWordsPerClass.get(c).keySet()){
+                totalWordsPerClass.put(c, totalWordsPerClass.get(c) + countWordsPerClass.get(c).get(s));
+            }
+        }
+
+        //calculate actual prior chances
+        for(String c : C){
+            for(String s : countWordsPerClass.get(c).keySet()){
+                double val = (countWordsPerClass.get(c).get(s) + K) / (totalWordsPerClass.get(c) + K + V.size());
+                chanceOnWordGivenClass.get(c).put(s, val);
+            }
+        }
+    }
+
+    public String classify(Map<String, Integer> doc){
+        double max = 0;
+        String bestClass = null;
+
+        for(String c : C){
+            double val = CPrior.get(c);
+            for(String s : doc.keySet()){
+                if(chanceOnWordGivenClass.get(c).containsKey(s)){
+                    val = val * (float) (Math.pow(chanceOnWordGivenClass.get(c).get(s), doc.get(s)));
+                }
+            }
+            if(max < val){
+                bestClass = c;
+                max = val;
+            }
+        }
+
+        return bestClass;
     }
 }
--- src/main/java/nl/epicspray/AI/Main.java
+++ src/main/java/nl/epicspray/AI/Main.java
@@ -2,11 +2,44 @@ package nl.epicspray.AI;
 
 import nl.epicspray.AI.util.SystemController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Gebruiker on 14-1-2017.
  */
 public class Main {
     public static void main(String[] args) {
         SystemController.getLogger().debug("Test");
+        Map<Map<String,Integer>, String> training = new HashMap<Map<String, Integer>, String>();
+        Map<String, Integer> file1 = new HashMap<String, Integer>();
+        file1.put("Uganda", 2);
+        file1.put("Bank", 1);
+        Map<String, Integer> file2 = new HashMap<String, Integer>();
+        file2.put("Uganda", 2);
+        file2.put("Solar", 1);
+        Map<String, Integer> file3 = new HashMap<String, Integer>();
+        file3.put("Uganda", 1);
+        file3.put("Mexico", 1);
+        Map<String, Integer> file4 = new HashMap<String, Integer>();
+        file4.put("Uganda", 1);
+        file4.put("Japan", 1);
+        file4.put("Tokyo", 1);
+        Map<String, Integer> file5 = new HashMap<String, Integer>();
+        file5.put("Uganda", 3);
+        file5.put("Tokyo", 1);
+        file5.put("Japan", 1);
+        training.put(file1, "Spam");
+        training.put(file2, "Spam");
+        training.put(file3, "Spam");
+        training.put(file4, "Ham");
+        List<String> classes = new ArrayList<String>();
+        classes.add("Spam");
+        classes.add("Ham");
+        Bayes bayes = new Bayes();
+        bayes.train(classes, training);
+        SystemController.getLogger().debug(bayes.classify(file5));
     }
 }
